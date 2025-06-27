14f6a2f523cf2a46f8fdac285d6d878638a7a9ab
paul-harley <paulharley2016@gmail.com>
2025-06-27 20:51:33 +0100

    start of graphics

--- Chess/Chess.cpp
+++ Chess/Chess.cpp
@@ -14,7 +14,7 @@ void printVecPair(std::vector<std::pair<int, int>> myVec) {
 }
 
 
-int main()
+int main1()
 {
     Board myBoard = Board();
     myBoard.printBoard();
@@ -26,6 +26,85 @@ int main()
 
     printVecPair(myBoard.possibleMovesAt(4,2));
 
+    return 0;
+
 }
+
+#include <SFML/Graphics.hpp>
+
+void drawBoard(sf::RenderWindow& window, Board& board) {
+
+    //sets whole window to this colour
+    sf::Color backgroundColour(0xADD8E6); // light blue
+    window.clear(backgroundColour);
+
+    int squareSize = 60;
+
+    //add all squares
+    //moves from top left to bottom right
+    bool isWhite = true;
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+
+            //drawing square background
+            sf::RectangleShape shape(sf::Vector2f{ static_cast<float>(squareSize), static_cast<float>(squareSize)});
+
+            if (isWhite == true) {
+                shape.setFillColor(sf::Color::White);
+                //last colour of one row is same as start on next
+                if (j != 7) {
+                    isWhite = false;
+                }
+            }
+            else {
+                shape.setFillColor(sf::Color::Black);
+                //last colour of one row is same as start on next
+                if (j != 7) {
+                    isWhite = true;
+                }
+            }
+            shape.setPosition(sf::Vector2f{static_cast<float>(squareSize*j + 60), static_cast<float>(squareSize* i + 60) });
+            //window.draw(shape);
+            //add piece if there is one
+
+            char piece = board.typeOfPieceAtCords(i, j);
+            if (piece != ' ') {
+                shape.setFillColor(sf::Color::Red);
+            }
+            window.draw(shape);
+
+        }
+    }
+
+    window.display();
+
+
+}
+
+int main() {
+
+
+    Board myBoard = Board();
+    myBoard.printBoard();
+    sf::RenderWindow window(sf::VideoMode({ 600, 700 }), "CHESS!");
+
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        drawBoard(window, myBoard);
+
+    }
+
+
+    return 0;
+}
+
+
 
 
