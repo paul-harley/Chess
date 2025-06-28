3f753d5090903e409845cabc076b210e83414b58
paul-harley <paulharley2016@gmail.com>
2025-06-28 22:34:22 +0100

    pieces added for graphics

--- Chess/Chess.cpp
+++ Chess/Chess.cpp
@@ -32,10 +32,77 @@ int main1()
 
 #include <SFML/Graphics.hpp>
 
+string spriteFileToDraw(Board& board, int row, int col) {
+
+    //add piece if there is one
+    char piece = board.typeOfPieceAtCords(row, col);
+
+    switch (piece)
+    {
+
+    case 'p':
+        if (board.board[row][col].get()->colour == Colour::BLACK) {
+            return "Images/blackPawn.png";
+        }
+        else {
+            return "Images/whitePawn.png";
+        }
+
+    case 'n':
+        if (board.board[row][col].get()->colour == Colour::BLACK) {
+            return "Images/blackKnight.png";
+        }
+        else {
+            return "Images/whiteKnight.png";
+        }
+
+    case 'b':
+        if (board.board[row][col].get()->colour == Colour::BLACK) {
+            return "Images/blackBishop.png";
+        }
+        else {
+            return "Images/whiteBishop.png";
+        }
+
+    case 'r':
+        if (board.board[row][col].get()->colour == Colour::BLACK) {
+            return "Images/blackRook.png";
+        }
+        else {
+            return "Images/whiteRook.png";
+        }
+
+    case 'q':
+        if (board.board[row][col].get()->colour == Colour::BLACK) {
+            return "Images/blackQueen.png";
+        }
+        else {
+            return "Images/whiteQueen.png";
+        }
+
+    case 'k':
+        if (board.board[row][col].get()->colour == Colour::BLACK) {
+            return "Images/blackKing.png";
+        }
+        else {
+            return "Images/whiteKing.png";
+        }
+
+
+
+    default:
+        return "";
+    }
+
+}
+
 void drawBoard(sf::RenderWindow& window, Board& board) {
 
-    //sets whole window to this colour
-    sf::Color backgroundColour(0xADD8E6); // light blue
+    sf::Color backgroundColour(173, 216, 230); // light blue
+    sf::Color lightSquares(196, 164, 132); //light brown
+    sf::Color darkSquares(128, 101, 23); //oak
+
+
     window.clear(backgroundColour);
 
     int squareSize = 60;
@@ -46,33 +113,36 @@ void drawBoard(sf::RenderWindow& window, Board& board) {
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
 
+            sf::Vector2f currentPos{ static_cast<float>(squareSize * j + 60), static_cast<float>(squareSize * i + 60) };
+
             //drawing square background
             sf::RectangleShape shape(sf::Vector2f{ static_cast<float>(squareSize), static_cast<float>(squareSize)});
 
             if (isWhite == true) {
-                shape.setFillColor(sf::Color::White);
+                shape.setFillColor(darkSquares);
                 //last colour of one row is same as start on next
                 if (j != 7) {
                     isWhite = false;
                 }
             }
             else {
-                shape.setFillColor(sf::Color::Black);
+                shape.setFillColor(lightSquares);
                 //last colour of one row is same as start on next
                 if (j != 7) {
                     isWhite = true;
                 }
             }
-            shape.setPosition(sf::Vector2f{static_cast<float>(squareSize*j + 60), static_cast<float>(squareSize* i + 60) });
-            //window.draw(shape);
-            //add piece if there is one
-
-            char piece = board.typeOfPieceAtCords(i, j);
-            if (piece != ' ') {
-                shape.setFillColor(sf::Color::Red);
-            }
+            shape.setPosition(currentPos);
             window.draw(shape);
-
+            
+            //add piece if there is one
+            string pieceFileName = spriteFileToDraw(board, i, j);
+            if (pieceFileName.length() > 0) {
+                const sf::Texture piece(pieceFileName);
+                sf::Sprite sprite(piece);
+                sprite.setPosition(currentPos);
+                window.draw(sprite);
+            }
         }
     }
 
@@ -80,6 +150,8 @@ void drawBoard(sf::RenderWindow& window, Board& board) {
 
 
 }
+
+
 
 int main() {
 
